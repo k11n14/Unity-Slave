f5f75fb3e364c8fd16a4c718b9da5bf5cb0fbf5a
ONITCH <onisoldier0705@gmail.com>
2023-01-06 14:40:17 +0900

    マルチプレイのベース追加

---
+++ Assets/Scripts/Movement/CharacterMovementHandler.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class CharacterMovementHandler : NetworkBehaviour
+{
+    //Other components
+    NetworkCharacterControllerPrototypeCustom networkCharacterControllerPrototypeCustom;
+
+    private void Awake()
+    {
+        networkCharacterControllerPrototypeCustom = GetComponent<NetworkCharacterControllerPrototypeCustom>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        //Get the input from the network
+        if (GetInput(out NetworkInputData networkInputData))
+        {
+            //Rotate the transform according to the client aim vector
+            transform.forward = networkInputData.aimForwardVector;
+
+            //Cancel out rotation on X axis as we don't want our character to tilt
+            Quaternion rotation = transform.rotation;
+            rotation.eulerAngles = new Vector3(0, rotation.eulerAngles.y, rotation.eulerAngles.z);
+            transform.rotation = rotation;
+
+            //Move
+            Vector3 moveDirection = transform.forward * networkInputData.movementInput.y + transform.right * networkInputData.movementInput.x;
+            moveDirection.Normalize();
+
+            networkCharacterControllerPrototypeCustom.Move(moveDirection);
+
+            //Jump
+            if(networkInputData.isJumpPressed)
+                networkCharacterControllerPrototypeCustom.Jump();
+
+            //Check if we've fallen off the world.
+            CheckFallRespawn();
+        }
+
+    }
+
+    void CheckFallRespawn()
+    {
+        if (transform.position.y < -12)
+            transform.position = Utils.GetRandomSpawnPoint();
+    }
+
+}
---
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class NetworkPlayer : NetworkBehaviour, IPlayerLeft
+{
+    public static NetworkPlayer Local { get; set; }
+
+    public Transform playerModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void Spawned()
+    {
+        if (Object.HasInputAuthority)
+        {
+            Local = this;
+
+            //Sets the layer of the local players model
+            Utils.SetRenderLayerInChildren(playerModel, LayerMask.NameToLayer("LocalPlayerModel"));
+
+            //Disable main camera
+            Camera.main.gameObject.SetActive(false);
+
+            Debug.Log("Spawned local player");
+        }
+        else
+        {
+            //Disable the camera if we are not the local player
+            Camera localCamera = GetComponentInChildren<Camera>();
+            localCamera.enabled = false;
+
+            //Only 1 audio listner is allowed in the scene so disable remote players audio listner
+            AudioListener audioListener = GetComponentInChildren<AudioListener>();
+            audioListener.enabled = false;
+
+            Debug.Log("Spawned remote player");
+        }
+
+        //Make it easier to tell which player is which.
+        transform.name = $"P_{Object.Id}";
+    }
+
+    public void PlayerLeft(PlayerRef player)
+    {
+        if (player == Object.InputAuthority)
+            Runner.Despawn(Object);
+
+    }
+}
